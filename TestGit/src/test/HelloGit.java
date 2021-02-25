d26b8588b6927264e760f4b7d601c75ddd636bc6
mlk <rjun912@gmail.com>
2021-02-25 10:43:33 +0530

    updations done my Mallikarjun

--- TestGit/src/test/HelloGit.java
+++ TestGit/src/test/HelloGit.java
@@ -3,6 +3,7 @@ package test;
 public class HelloGit {
 	public static void main(String[] args) {
 		int x=200;
-		System.out.println("x = "+ x); 
+		System.out.println("x = "+ x);
+		System.out.println("Hello this is a sample git repository");
 	}
 }
