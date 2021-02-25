a00039f3dc2589566ce53d6f8f1f31bad304ffb2
mlk <rjun912@gmail.com>
2021-02-25 11:16:28 +0530

    second commit

--- TestGit/src/test/TestClass.java
+++ TestGit/src/test/TestClass.java
@@ -2,6 +2,7 @@ package test;
 
 public class TestClass {
 	public static void main(String[] args) {
-		System.out.println("new class");  
+		System.out.println("new class"); 
+		System.out.println("Updated in TestClass");
 	}
 }
